21e1d35ae52860a15d65a3b1e33ca9bfdf891e23
v.shalginov <slava8427@mail.ru>
2021-05-10 16:44:06 +0700

    change price

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import { useState } from "react";
 import { PizzaConfigurator } from "./PizzaConfigurator";
 import { PizzaOrderCheckouter } from "./PizzaOrderCheckouter";
+const BASEPRICE = 200;
+const SIZEADDTOPRICE = 50;
+const TOPPINGADDTOPRICE = 39;
 
 function App() {
   const [isConfiguringPizza, setIsConfiguringPizza] = useState(true);
   const [order, setOrder] = useState({})
-
-  if (isConfiguringPizza) return <PizzaConfigurator setOrder={setOrder} setIsConfiguringPizza={setIsConfiguringPizza} />;
+  
+  if (isConfiguringPizza) return <PizzaConfigurator constProps={{BASEPRICE, SIZEADDTOPRICE, TOPPINGADDTOPRICE}} orderProp={{order, setOrder}} setIsConfiguringPizza={setIsConfiguringPizza} />;
   if (!isConfiguringPizza) return <PizzaOrderCheckouter order={order} setIsConfiguringPizza={setIsConfiguringPizza} />;
 }
 
--- src/PizzaConfigurator.js
+++ src/PizzaConfigurator.js
@@ -1,40 +1,60 @@
-import {useState} from "react"
+import { useState } from "react"
 
-export const PizzaConfigurator = ({setIsConfiguringPizza, setOrder}) => {
-    const [price, setPrice] = useState(200)    
+export const PizzaConfigurator = ({ setIsConfiguringPizza, orderProp: { order, setOrder },
+     constProps: { BASEPRICE, SIZEADDTOPRICE, TOPPINGADDTOPRICE } }) => {
+    const [price, setPrice] = useState(BASEPRICE)
     const onSubmitHandler = (event) => {
         event.preventDefault()
-        setIsConfiguringPizza(prev=>!prev)
-        setOrder(
-            event.target
-        )
+        setIsConfiguringPizza(prev => !prev)
+        const form = event.target
+        console.log("size ", form.size);
+        if (form.size.valu == 35) setPrice(prev => prev + 50)
+
     }
 
-    return <form onSubmit={onSubmitHandler}>
+    const onChangeIput = (name, value) => {
+        setOrder(prev => {
+            return { ...prev, [name]: value }
+        })
+        console.log("order:", order);
+        changePrice()
+    }
+
+    const changePrice = () => {
+        let newPrice = BASEPRICE;
+        Object.values(order).map(value => {
+            console.log("value", value);
+            if(value==35) newPrice += SIZEADDTOPRICE
+            newPrice += TOPPINGADDTOPRICE
+        })
+        setPrice(newPrice)
+    }
+
+    return <form onSubmit={onSubmitHandler} onChange={(e) => onChangeIput(e.target.name, e.target.value)}>
         <fieldset>
             <legend>Configure your pizza</legend>
             <legend>Pizza size</legend>
-                <label><input name="size" type="radio" value="30"/>30</label>
-                <label><input name="size" type="radio" value="35"/>35</label>
+            <label><input name="size" type="radio" value="30" />30</label>
+            <label><input name="size" type="radio" value="35" />35</label>
             <legend>Dough</legend>
-                <label><input name="dough" type="radio" value="thick"/>thick</label>
-                <label><input name="dough" type="radio" value="fat"/>fat</label>
+            <label><input name="dough" type="radio" value="thick" />thick</label>
+            <label><input name="dough" type="radio" value="fat" />fat</label>
             <legend>Souse</legend>
-                <label><input name="souse" type="radio" value="tomato"/>tomato</label>
-                <label><input name="souse" type="radio" value="white"/>white</label>
-                <label><input name="souse" type="radio" value="spicy"/>spicy</label>
+            <label><input name="souse" type="radio" value="tomato" />tomato</label>
+            <label><input name="souse" type="radio" value="white" />white</label>
+            <label><input name="souse" type="radio" value="spicy" />spicy</label>
             <legend>Cheese</legend>
-                <label><input name="cheese" type="radio" value="mozarella"/>mozarella</label>
-                <label><input name="cheese" type="radio" value="chedder"/>chedder</label>
-                <label><input name="cheese" type="radio" value="dorblu"/>dorblu</label>
-            <legend>Vegetables</legend>
-                <label><input name="vegetables" type="radio" value="tomato"/>tomato</label>
-                <label><input name="vegetables" type="radio" value="mushrooms"/>mushrooms</label>
-                <label><input name="vegetables" type="radio" value="pepper"/>pepper</label>
+            <label><input name="cheese" type="radio" value="mozarella" />mozarella</label>
+            <label><input name="cheese" type="radio" value="chedder" />chedder</label>
+            <label><input name="cheese" type="radio" value="dorblu" />dorblu</label>
+            <legend>Vegetables</legend>s
+                <label><input name="vegetables" type="radio" value="tomato" />tomato</label>
+            <label><input name="vegetables" type="radio" value="mushrooms" />mushrooms</label>
+            <label><input name="vegetables" type="radio" value="pepper" />pepper</label>
             <legend>Meat</legend>
-                <label><input name="meat" type="radio" value="bacon"/>bacon</label>
-                <label><input name="meat" type="radio" value="peperoni"/>peperoni</label>
-                <label><input name="meat" type="radio" value="ham"/>ham</label>
+            <label><input name="meat" type="radio" value="bacon" />bacon</label>
+            <label><input name="meat" type="radio" value="peperoni" />peperoni</label>
+            <label><input name="meat" type="radio" value="ham" />ham</label>
         </fieldset>
         <button type="submit">Order for: {price}$</button>
     </form>
