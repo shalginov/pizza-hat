64ee7c69646ab1892f19ff75e87300044d13b667
VS <slava8427@mail.ru>
2021-05-11 21:22:24 +0700

    added App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it.todo("renders PizzaConfigurator, PizzaOrderCheckouter it depends of state isConfiguringPizza");
+
+describe("render App's components", ()=>{
+  it("render PizzaConfigurator, cause isConfiguringPizza state is true", ()=>{
+    //Arrange
+    //Act
+    const {getByText} = render(<App/>)
+    const pizzaConfigurator = getByText("Configure your pizza")
+    //Assert
+    expect(pizzaConfigurator).toBeInTheDocument()
+  })
+})
